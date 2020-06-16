9fba1ed79dd1020d790042fd9ae8bf6b67637cac
Andrey Bezpalenko <virtustilus@gmail.com>
2020-06-16 07:41:54 +0300

    Initial Commit

---
+++ MacLangSwitcherForVNC/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLangSwitcherForVNC
+//
+//  Created by Vision on 16.06.2020.
+//  Copyright © 2020 Virtustilus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
